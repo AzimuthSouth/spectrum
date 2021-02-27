fix merge averaging

test/test_analyse.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy
+import scipy
 
 from staff import analyse
 from staff import prepare
@@ -14,18 +15,7 @@ class TestAnalyse(unittest.TestCase):
         f, g = analyse.spectrum_density(x, len(x), len(x))
         fm = f[numpy.where(g == max(g))][0]
         self.assertEqual((max(g) - a ** 2 / 2) < 1.0e-7 and (f0 - fm < 1.0e-7), True,
-                         "phase or spectrum density failed")
-
-    def test_spectrum_density_harmonic_weighting(self):
-        t = numpy.arange(0, 10, 0.01)
-        f0 = 2.0
-        a = 3.0
-        x = a * numpy.cos(2 * numpy.pi * f0 * t)
-        xr = prepare.correction(prepare.smoothing(x, 3), 2)
-        f, g = analyse.spectrum_density(xr, len(xr), len(xr))
-        fm = f[numpy.where(g == max(g))][0]
-        self.assertEqual((max(g) - a ** 2 / 2) < 1.0e-7 and (f0 - fm < 1.0e-7), True,
-                         "phase or spectrum density failed")
+                         "spectrum density failed")
 
     def test_n_coefficient(self):
         z = [1.0 + 1.0j, 1.0, -1.0, -1.0 + 1.0j, -1.0 - 1.0j, 1.0j, -1.0j]
@@ -36,7 +26,7 @@ class TestAnalyse(unittest.TestCase):
             eps += (res[i] - ans[i]) ** 2
         self.assertEqual(eps ** 0.5 < 1.0e-7, True, "n_coefficient test failed")
 
-    def test_mod_fas(self):
+    def test_mod_phase(self):
         z = [2.0 ** 0.5, 1.0 + 1.0j, 2 ** 0.5 * 1.0j, -1.0 + 1.0j, -2.0 ** 0.5, -1.0 - 1.0j, -2 ** 0.5 * 1.0j,
              1.0 - 1.0j]
         [m, f] = analyse.cross_spectrum_mod_fas(z)
@@ -46,6 +36,12 @@ class TestAnalyse(unittest.TestCase):
         eps1 = numpy.linalg.norm(mod - m)
         eps2 = numpy.linalg.norm(fas - f)
         self.assertEqual(eps1 < 1.0e-7 and eps2 < 1.0e-7, True, "module or phase calculation failed")
+
+    def test_frequencies(self):
+        res = analyse.frequencies(100, 0.01)
+        ans = scipy.fft.rfftfreq(100, 0.01)
+        eps = numpy.sum((res - ans) ** 2)
+        self.assertEqual(eps ** 0.5 < 1.0e-7, True, "frequencies test failed")
 
 
 if __name__ == '__main__':